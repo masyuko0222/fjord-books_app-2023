67763ed504049e0f319f2debd5eaa2934bebec84
masyuko0222 <masyuko0222@gmail.com>
2023-03-28 23:18:52 +0900

    追加　ストロングパラメータの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # ログイン済ユーザーのみアクセスを許可
   before_action :authenticate_user!
+
+  # deviseコントローラにストロングパラメータを追加
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:post_code, :address, :introduction])
+ 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:post_code, :address, :introduction])
+  end
 end
