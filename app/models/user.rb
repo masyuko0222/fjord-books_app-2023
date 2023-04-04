de635abf01ce7bb34c1a495a14c0a9cf03424d6a
masyuko0222 <masyuko0222@gmail.com>
2023-04-04 21:05:17 +0900

    feat users#editで、アイコン画像のアップロード機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ActiveStorageとUserモデルの紐づけ
+  has_one_attached :icon
 end
