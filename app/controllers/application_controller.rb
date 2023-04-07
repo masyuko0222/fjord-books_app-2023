251d58d9dbbe9a6f277396612c78ecec285dc1ed
masyuko0222 <masyuko0222@gmail.com>
2023-04-08 00:33:17 +0900

    Feat: postcode,address,profileにStrongParametersを適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@
 class ApplicationController < ActionController::Base
   # ページを閲覧するには、ログインを必須に設定
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[zipcode address profile])
+  end
 end
