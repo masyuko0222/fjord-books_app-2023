36cd34bd5ff02585c7adf0f5c416360e25e212e7
masyuko0222 <masyuko0222@gmail.com>
2023-05-05 17:01:14 +0900

    Fix: CommentのDestroy時、Commentの投稿者(User)かの確認を、Controllerのbefore_actionで実装

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -2,11 +2,18 @@
 
 class Books::CommentsController < CommentsController
   before_action :set_commentable
+  before_action :correct_user, only: %i[destroy]
 
   private
 
   def set_commentable
     @commentable = Book.find(params[:book_id])
+  end
+
+  def correct_user
+    unless current_user == @commentable.user
+      redirect_to root_path
+    end
   end
 
   def render_commentable_show
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -2,11 +2,18 @@
 
 class Reports::CommentsController < CommentsController
   before_action :set_commentable
+  before_action :correct_user, only: %i[destroy]
 
   private
 
   def set_commentable
     @commentable = Report.find(params[:report_id])
+  end
+
+  def correct_user
+    unless current_user == @commentable.user
+      redirect_to root_path
+    end
   end
 
   def render_commentable_show
