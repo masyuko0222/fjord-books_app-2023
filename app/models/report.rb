18917bf9d0e5d9ab9c61840bd2055144cae714f1
masyuko0222 <masyuko0222@gmail.com>
2023-05-12 22:51:20 +0900

    更新：　Report → through Mentions → Reportの関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,7 +5,9 @@ class Report < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
 
   has_many :from_relationships, class_name: 'Mention', foreign_key: 'mention_from_id', dependent: :destroy
+  has_many :mentioning_reports, through: :from_relationships, source: :mention_to
   has_many :to_relationships, class_name: 'Mention', foreign_key: 'mention_to_id', dependent: :destroy
+  has_many :mentioned_reports, through: :to_relationships, source: :mention_from
 
   validates :title, presence: true
   validates :content, presence: true
