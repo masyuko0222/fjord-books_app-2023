bd2aebaaf34f543e24d457e335c5036a530dbed2
masyuko0222 <masyuko0222@gmail.com>
2023-05-13 00:09:10 +0900

    更新：　Mentionモデルに空白禁止のバリデーション付与

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :mention_from, class_name: 'Report'
   belongs_to :mention_to, class_name: 'Report'
+  validates :mention_from, presence: true
+  validates :mention_to, presence: true
 end
