678b73701acc21bbc0a91f1ab775aaa2d3dad402
masyuko0222 <masyuko0222@gmail.com>
2023-05-12 21:36:47 +0900

    更新：　migrationファイルの適用。schema.rbの更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_111800) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,6 +57,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "mentions", force: :cascade do |t|
+    t.integer "mention_from_id"
+    t.integer "mention_to_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mention_from_id", "mention_to_id"], name: "index_mentions_on_mention_from_id_and_mention_to_id", unique: true
+    t.index ["mention_from_id"], name: "index_mentions_on_mention_from_id"
+    t.index ["mention_to_id"], name: "index_mentions_on_mention_to_id"
   end
 
   create_table "reports", force: :cascade do |t|
