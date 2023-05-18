d9c4514299ac3a8a80b9d99d970391aa1d7ea130
masyuko0222 <masyuko0222@gmail.com>
2023-05-18 23:43:39 +0900

    修正：　不要なinverse_ofオプションを削除

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Mention < ApplicationRecord
-  belongs_to :mention_from, class_name: 'Report', inverse_of: :from_relationships
-  belongs_to :mention_to, class_name: 'Report', inverse_of: :to_relationships
+  belongs_to :mention_from, class_name: 'Report'
+  belongs_to :mention_to, class_name: 'Report'
   validates :mention_from, presence: true
   validates :mention_to, presence: true
 end
