3ec215cc9931c92553cdda93f350f0675b204146
masyuko0222 <masyuko0222@gmail.com>
2023-05-20 17:59:36 +0900

    修正：　NOT NULL制約をDB上で付与するために、既存migrationファイルを修正し、DBに反映

--- db/migrate/20230512111800_create_mentions.rb
+++ db/migrate/20230512111800_create_mentions.rb
@@ -1,13 +1,11 @@
 class CreateMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :mentions do |t|
-      t.integer :mention_from_id
-      t.integer :mention_to_id
+      t.belongs_to :mention_from, null: false, foreign_key: { to_table: :reports }
+      t.belongs_to :mention_to, null: false, foreign_key: { to_table: :reports }
 
       t.timestamps
     end
-  add_index :mentions, :mention_from_id
-  add_index :mentions, :mention_to_id
   add_index :mentions, [:mention_from_id, :mention_to_id], unique: true
   end
 end
