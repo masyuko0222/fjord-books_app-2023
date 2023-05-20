6d5f7c1196388f195d78dbe346fc5404e63670e8
masyuko0222 <masyuko0222@gmail.com>
2023-05-21 00:39:36 +0900

    リファクタリング：　Reportモデルと中間テーブル関連付けの命名を変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,11 +4,11 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :from_relationships, class_name: 'Mention', foreign_key: 'mention_from_id', dependent: :destroy, inverse_of: :mention_from
-  has_many :mentioning_reports, through: :from_relationships, source: :mention_to
+  has_many :sending_mentions_relationships, class_name: 'Mention', foreign_key: 'mention_from_id', dependent: :destroy, inverse_of: :mention_from
+  has_many :mentioning_reports, through: :sending_mentions_relationships, source: :mention_to
 
-  has_many :to_relationships, class_name: 'Mention', foreign_key: 'mention_to_id', dependent: :destroy, inverse_of: :mention_to
-  has_many :mentioned_reports, through: :to_relationships, source: :mention_from
+  has_many :recieving_mentions_relationships, class_name: 'Mention', foreign_key: 'mention_to_id', dependent: :destroy, inverse_of: :mention_to
+  has_many :mentioned_reports, through: :recieving_mentions_relationships, source: :mention_from
 
   validates :title, presence: true
   validates :content, presence: true
