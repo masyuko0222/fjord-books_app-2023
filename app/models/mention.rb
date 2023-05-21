996c9c0135f8643e04f66db6bbf582187e5e971d
masyuko0222 <masyuko0222@gmail.com>
2023-05-21 22:41:35 +0900

    リファクタリング：　古いバリデーション文法を修正

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -4,5 +4,5 @@ class Mention < ApplicationRecord
   belongs_to :mention_from, class_name: 'Report'
   belongs_to :mention_to, class_name: 'Report'
 
-  validates_uniqueness_of :mention_from_id, scope: :mention_to_id
+  validates :mention_from_id, uniqueness: { scope: :mention_to_id }
 end
