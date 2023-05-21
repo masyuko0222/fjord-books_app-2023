059e6b2ebfe56701bc68333091209d43f6ef9026
masyuko0222 <masyuko0222@gmail.com>
2023-05-21 19:20:37 +0900

    修正：　belongs_toでは自動でNOT NULLとなるため、外部キーからnull: falseを削除しました

--- db/migrate/20230512111800_create_mentions.rb
+++ db/migrate/20230512111800_create_mentions.rb
@@ -1,8 +1,8 @@
 class CreateMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :mentions do |t|
-      t.belongs_to :mention_from, null: false, foreign_key: { to_table: :reports }
-      t.belongs_to :mention_to, null: false, foreign_key: { to_table: :reports }
+      t.belongs_to :mention_from, foreign_key: { to_table: :reports }
+      t.belongs_to :mention_to, foreign_key: { to_table: :reports }
 
       t.timestamps
     end
