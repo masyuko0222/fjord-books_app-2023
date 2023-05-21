980bd97fa72d2344de03571f83fcab7a416a343b
masyuko0222 <masyuko0222@gmail.com>
2023-05-21 17:06:55 +0900

    修正：　Mentionモデルでも複合インデックスの一意性制限バリデーションを追加

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :mention_from, class_name: 'Report'
   belongs_to :mention_to, class_name: 'Report'
+
+  validates_uniqueness_of :mention_from_id, scope: :mention_to_id
 end
