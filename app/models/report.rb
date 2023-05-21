728d0404364066f4aa32eedc18f84a1ae16ecb75
masyuko0222 <masyuko0222@gmail.com>
2023-05-21 17:10:12 +0900

    リファクタリング：　関連付けの命名を変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,11 +6,11 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :sending_mentions_relationships, class_name: 'Mention', foreign_key: 'mention_from_id', dependent: :destroy, inverse_of: :mention_from
-  has_many :mentioning_reports, through: :sending_mentions_relationships, source: :mention_to
+  has_many :sending_mentions, class_name: 'Mention', foreign_key: 'mention_from_id', dependent: :destroy, inverse_of: :mention_from
+  has_many :mentioning_reports, through: :sending_mentions, source: :mention_to
 
-  has_many :recieving_mentions_relationships, class_name: 'Mention', foreign_key: 'mention_to_id', dependent: :destroy, inverse_of: :mention_to
-  has_many :mentioned_reports, through: :recieving_mentions_relationships, source: :mention_from
+  has_many :recieving_mentions, class_name: 'Mention', foreign_key: 'mention_to_id', dependent: :destroy, inverse_of: :mention_to
+  has_many :mentioned_reports, through: :recieving_mentions, source: :mention_from
 
   validates :title, presence: true
   validates :content, presence: true
