bfa98b4659433d70cfac507f78c7a89a262d3e2e
masyuko0222 <masyuko0222@gmail.com>
2023-05-31 21:17:42 +0900

    新規：　Reportモデルのeditable?メソッドのテストコード

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable? should return true if current user is writer of the report' do
+    report = reports(:alice_report) 
+    target_user = users(:alice)
+
+    assert(report.editable?(target_user))
+  end
+
+  test 'editable? should return false if current user is not writer of the report' do
+    report = reports(:bob_report) 
+    target_user = users(:alice)
+
+    assert_not(report.editable?(target_user))
+  end
 end
