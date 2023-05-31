fcef723caeebbe1a002d8b0ee02a76b027bcd88d
masyuko0222 <masyuko0222@gmail.com>
2023-05-31 20:22:41 +0900

    新規：　Userモデルのname_or_emailメソッドのテストコード

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return email if name was not registed" do
+    user = users(:no_name_has_email)
+
+    assert_equal 'foo@example.com', user.name_or_email
+  end
+
+  test "should return name if name was registed" do
+    user = users(:has_name_has_email)
+
+    assert_equal 'bar', user.name_or_email
+  end
 end
