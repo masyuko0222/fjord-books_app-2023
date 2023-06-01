1ca6f90aa31bcba4dad760335c323607e1878f69
masyuko0222 <masyuko0222@gmail.com>
2023-06-01 23:56:20 +0900

    リファクタリング：　rubocopの指摘点を反映

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -14,31 +14,29 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test 'editable? should return false if current user is not writer of the report' do
-    report = reports(:bob_report) 
+    report = reports(:bob_report)
     target_user = users(:alice)
 
     assert_not(report.editable?(target_user))
   end
 
   test 'created_on should convert created_at value to data object' do
-    @report.created_at = "Fri, 26 May 2023 21:10:12.289195000 JST +09:00"
+    @report.created_at = 'Fri, 26 May 2023 21:10:12.289195000 JST +09:00'
 
     assert_equal('Fri, 26 May 2023'.to_date, @report.created_on)
   end
 
   test 'save_mentions should save new mentions scanned from report content without own id' do
-    mention_regexp = %r{http://localhost:3000/reports/(\d+)}
-
-    @report.content = "http://localhost:3000/reports/2 is good. http://localhost:3000/reports/aaa is normal."
+    @report.content = 'http://localhost:3000/reports/2 is good. http://localhost:3000/reports/aaa is normal.'
 
     @report.send(:save_mentions)
 
     assert_equal([2], @report.mentioning_report_ids)
 
-    @report.content = "I am http://localhost:3000/reports/#{@report.id}. http://localhost:3000/reports/2 is good. http://localhost:3000/reports/3 is normal. http://localhost:3000/reports/4 is bad."
+    @report.content = "I am http://localhost:3000/reports/#{@report.id}. http://localhost:3000/reports/2 is good. http://localhost:3000/reports/3 is normal."
 
     @report.send(:save_mentions)
 
-    assert_equal([2,3,4], @report.mentioning_report_ids)
+    assert_equal([2, 3], @report.mentioning_report_ids)
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,13 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "name_or_email should return email if name was not registed" do
+  test 'name_or_email should return email if name was not registed' do
     user = users(:no_name_has_email)
 
     assert_equal 'foo@example.com', user.name_or_email
   end
 
-  test "name_or_email should return name if name was registed" do
+  test 'name_or_email should return name if name was registed' do
     user = users(:has_name_has_email)
 
     assert_equal 'bar', user.name_or_email
