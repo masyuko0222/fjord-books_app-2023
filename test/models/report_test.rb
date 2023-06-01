c36a866d86f3cffebb947d07e1e56f6434b9d3c1
masyuko0222 <masyuko0222@gmail.com>
2023-06-01 21:41:56 +0900

    リファクタリング：　Reportモデルのテストコードにsetupメソッドを導入

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,11 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  setup do
+    @report = reports(:alice_report)
+  end
+
   test 'editable? should return true if current user is writer of the report' do
-    report = reports(:alice_report) 
     target_user = users(:alice)
 
-    assert(report.editable?(target_user))
+    assert(@report.editable?(target_user))
   end
 
   test 'editable? should return false if current user is not writer of the report' do
@@ -18,9 +21,8 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test 'created_on should convert created_at value to data object' do
-    report = reports(:alice_report)
-    report.created_at = "Fri, 26 May 2023 21:10:12.289195000 JST +09:00"
+    @report.created_at = "Fri, 26 May 2023 21:10:12.289195000 JST +09:00"
 
-    assert_equal('Fri, 26 May 2023'.to_date, report.created_on)
+    assert_equal('Fri, 26 May 2023'.to_date, @report.created_on)
   end
 end
