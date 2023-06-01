1adad2601bc40aaf9ed2f9c4039422ebaca6069d
masyuko0222 <masyuko0222@gmail.com>
2023-06-01 21:25:18 +0900

    変更：　created_onメソッドのテストコードの命名と期待値を変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -17,9 +17,10 @@ class ReportTest < ActiveSupport::TestCase
     assert_not(report.editable?(target_user))
   end
 
-  test 'created_on should return Date object' do
+  test 'created_on should convert created_at value to data object' do
     report = reports(:alice_report)
+    report.created_at = "Fri, 26 May 2023 21:10:12.289195000 JST +09:00"
 
-    assert_instance_of(Date, report.created_on)
+    assert_equal('Fri, 26 May 2023'.to_date, report.created_on)
   end
 end
