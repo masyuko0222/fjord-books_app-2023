2b1c319be94159a50455807c4779bce48bcea372
masyuko0222 <masyuko0222@gmail.com>
2023-06-02 19:13:59 +0900

    微修正：　インスタンスメソッドの場合は、#を付けるように修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -7,26 +7,26 @@ class ReportTest < ActiveSupport::TestCase
     @report = reports(:alice_report)
   end
 
-  test 'editable? should return true if current user is writer of the report' do
+  test '#editable? should return true if current user is writer of the report' do
     target_user = users(:alice)
 
     assert(@report.editable?(target_user))
   end
 
-  test 'editable? should return false if current user is not writer of the report' do
+  test '#editable? should return false if current user is not writer of the report' do
     report = reports(:bob_report)
     target_user = users(:alice)
 
     assert_not(report.editable?(target_user))
   end
 
-  test 'created_on should convert created_at value to data object' do
+  test '#created_on should convert created_at value to data object' do
     @report.created_at = 'Fri, 26 May 2023 21:10:12.289195000 JST +09:00'
 
     assert_equal('Fri, 26 May 2023'.to_date, @report.created_on)
   end
 
-  test 'save_mentions should save new mentions scanned from report content without own id' do
+  test '#save_mentions should save new mentions scanned from report content without own id' do
     @report.content = 'http://localhost:3000/reports/2 is good. http://localhost:3000/reports/aaa is normal.'
 
     @report.send(:save_mentions)
