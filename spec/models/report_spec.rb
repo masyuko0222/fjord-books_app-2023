573534c9b798b07069530d472f4177cf949432a8
masyuko0222 <masyuko0222@gmail.com>
2023-06-07 20:55:39 +0900

    追加：　reportモデル　save_mentionsのテストコードをreport新規作成の場合のみ作成。まだ汚い。

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -35,4 +35,47 @@ RSpec.describe Report, type: :model do
       expect(report.created_on.class).to eq Date
     end
   end
+
+  describe '#save_mentions called after save' do
+    context '日報を新規作成する場合' do
+      context '正常な日報URIのみに対して言及すると' do
+        it '言及した日報全てをメンション関係に追加する' do
+          report = FactoryBot.create(:report, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good!")
+          mentioning_report_2 = FactoryBot.create(:report, id: 2)
+          mentioning_report_3 = FactoryBot.create(:report, id: 3)
+
+          report.save
+
+          expect(report.mentioning_report_ids).to eq [2,3]
+        end
+      end
+
+      context '異常な日報URIも含めて言及すると' do
+        it '正常な日報URIのみをメンション関係に追加する' do
+          report = FactoryBot.create(:report, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good! http://localhost:3000/reports/ijou is ijou. http://localhost:3000/reports/７７７ is zenkaku. http://localhost:3000/reports/290971 is not exist.")
+          mentioning_report_2 = FactoryBot.create(:report, id: 2)
+          mentioning_report_3 = FactoryBot.create(:report, id: 3)
+
+          report.save
+
+          expect(report.mentioning_report_ids).to eq [2,3]
+        end
+      end
+
+      context '自身の日報URIも含めて言及すると' do
+        it '自身の日報URIは除外してメンション関係に追加する' do
+          report = FactoryBot.create(:report, id: 1, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good! http://localhost:3000/reports/1 is me.")
+          mentioning_report_2 = FactoryBot.create(:report, id: 2)
+          mentioning_report_3 = FactoryBot.create(:report, id: 3)
+
+          report.save
+
+          expect(report.mentioning_report_ids).to eq [2,3]
+        end
+      end
+    end
+
+#    context '日報を更新する場合' do
+#    end
+  end
 end
