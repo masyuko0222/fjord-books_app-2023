0b130ab3ae47b618aa31e40085c4c55ce1ef0118
masyuko0222 <masyuko0222@gmail.com>
2023-06-07 23:15:37 +0900

    リファクタリング：　letの利用、contextのネストを一段階浅く

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -8,74 +8,65 @@ RSpec.describe Report, type: :model do
   end
 
   describe '#edit_able?' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:report) { FactoryBot.create(:report, user:) }
+
     context 'ログインしているユーザーが、レポートの作成者の場合' do
       it 'trueを戻り値とする' do
-        user = FactoryBot.create(:user)
-        report = FactoryBot.create(:report, user:)
-
         expect(report.editable?(user)).to be_truthy
       end
     end
 
     context 'ログインしているユーザーが、レポートの作成者ではない場合' do
       it 'falseを戻り値とする' do
-        user = FactoryBot.create(:user)
-        other_user = FactoryBot.create(:user)
-        report = FactoryBot.create(:report, user: other_user)
+        other_user = FactoryBot.create(:user, email: "otheruser@example.com")
+        report_created_by_other_user = FactoryBot.create(:report, user: other_user)
 
-        expect(report.editable?(user)).to be_falsey
+        expect(report_created_by_other_user.editable?(user)).to be_falsey
       end
     end
   end
 
   describe '#created_on' do
-    it 'created_atをDateオブジェクトに変換する' do
-      report = FactoryBot.create(:report)
+    let(:user) { FactoryBot.create(:user) }
+    let(:report) { FactoryBot.create(:report, user:) }
 
+    it 'created_atをDateオブジェクトに変換する' do
       expect(report.created_on.class).to eq Date
     end
   end
 
   describe '#save_mentions called after save' do
+    let!(:mentioning_report_2) { FactoryBot.create(:report, id:2) }
+    let!(:mentioning_report_3) { FactoryBot.create(:report, id:3) }
+
     context '日報を新規作成する場合' do
-      context '正常な日報URIのみに対して言及すると' do
-        it '言及した日報全てをメンション関係に追加する' do
-          report = FactoryBot.create(:report, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good!")
-          mentioning_report_2 = FactoryBot.create(:report, id: 2)
-          mentioning_report_3 = FactoryBot.create(:report, id: 3)
+      it '正常な日報URIのみに対して言及すると、言及した日報全てをメンション関係に追加する' do
+        report = FactoryBot.create(:report, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good!")
 
-          report.save
+        report.save
 
-          expect(report.mentioning_report_ids).to eq [2,3]
-        end
+        expect(report.mentioning_report_ids).to eq [2,3]
       end
 
-      context '異常な日報URIも含めて言及すると' do
-        it '正常な日報URIのみをメンション関係に追加する' do
-          report = FactoryBot.create(:report, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good! http://localhost:3000/reports/ijou is ijou. http://localhost:3000/reports/７７７ is zenkaku. http://localhost:3000/reports/290971 is not exist.")
-          mentioning_report_2 = FactoryBot.create(:report, id: 2)
-          mentioning_report_3 = FactoryBot.create(:report, id: 3)
+      it '異常な日報URIも含んで言及すると、正常な日報URIのみをメンション関係に追加する' do
+        non_exist_report_id = (Report.last.id).to_i + 1
 
-          report.save
+        report = FactoryBot.create(:report, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good! http://localhost:3000/reports/ijou is string. http://localhost:3000/reports/７７７ is zenkaku. http://localhost:3000/reports/#{non_exist_report_id} is not exist.")
 
-          expect(report.mentioning_report_ids).to eq [2,3]
-        end
+        report.save
+
+        expect(report.mentioning_report_ids).to eq [2,3]
       end
 
-      context '自身の日報URIも含めて言及すると' do
-        it '自身の日報URIは除外してメンション関係に追加する' do
-          report = FactoryBot.create(:report, id: 1, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good! http://localhost:3000/reports/1 is me.")
-          mentioning_report_2 = FactoryBot.create(:report, id: 2)
-          mentioning_report_3 = FactoryBot.create(:report, id: 3)
+      it '自身の日報URIも含めて言及すると、自身の日報URIは除外してメンション関係に追加する' do
+        # 今後letでreportのセットアップをしてもreport_idが重複しないよう、idに100を振る。
+        report_mentioning_self = FactoryBot.create(:report, id: 100, content: "http://localhost:3000/reports/2 is bad. http://localhost:3000/reports/3 is good! http://localhost:3000/reports/100 is me.")
 
-          report.save
+        report_mentioning_self.save
 
-          expect(report.mentioning_report_ids).to eq [2,3]
-        end
+        expect(report_mentioning_self.mentioning_report_ids).to eq [2,3]
       end
     end
-
-#    context '日報を更新する場合' do
-#    end
   end
 end
