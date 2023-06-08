d398a956bfb6cb272a0d24a1a63a8323f2fef70a
masyuko0222 <masyuko0222@gmail.com>
2023-06-08 22:13:36 +0900

    修正：　other_userをtraitに追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,5 +10,10 @@ FactoryBot.define do
     trait :user_without_name do
       name { nil }
     end
+
+    trait :other_user do
+      email { 'otheruser@example.com' }
+      name { 'Bob' }
+    end
   end
 end
