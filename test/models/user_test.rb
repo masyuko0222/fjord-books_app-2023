017fce3b8e35d2d7e53ff5787c924dd560c83f5b
masyuko0222 <masyuko0222@gmail.com>
2023-06-10 02:32:16 +0900

    変更：　UserモデルテストをSpecs形式に変更

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,18 @@
-# frozen_string_literal: true
-
-require 'test_helper'
+require "test_helper"
+require "minitest/autorun"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe '#name_or_email' do
+    it 'returns name if user has name' do
+      user = User.new(email: 'alice@example.com', name: 'alice')
+
+      assert_equal 'alice', user.name_or_email
+    end
+
+    it 'returns email if user dose not have name' do
+      user = User.new(email: 'alice@example.com', name: '')
+
+      assert_equal 'alice@example.com', user.name_or_email
+    end
+  end
 end
