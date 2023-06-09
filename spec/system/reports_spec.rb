6870c0ea0877f7a0287b336f48564ded0770ad48
masyuko0222 <masyuko0222@gmail.com>
2023-06-09 23:17:58 +0900

    a

--- spec/system/reports_spec.rb
+++ spec/system/reports_spec.rb
@@ -5,5 +5,27 @@ RSpec.describe "Reports", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario 'ユーザーが新しい日報を書く' do
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    fill_in 'Eメール', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
+
+    expect {
+      click_link '日報'
+      click_link '日報の新規作成'
+      fill_in 'タイトル', with: 'aaaa'
+      fill_in '内容', with: 'bbbb'
+      click_button '登録する'
+
+      expect(page).to have_content '日報が作成されました。'
+      expect(page).to have_content '日報の詳細'
+      expect(page).to have_content 'タイトル: aaaa'
+      expect(page).to have_content '内容: bbbb'
+      expect(page).to have_content "作成者: #{user.name}"
+      expect(page).to have_content "作成日: "
+    }.to change(user.reports, :count).by(1)
+  end
 end
