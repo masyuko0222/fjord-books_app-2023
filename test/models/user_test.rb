6c954113f6d334f8cc37b3a00a5cf246ccd3f465
masyuko0222 <masyuko0222@gmail.com>
2023-06-10 18:11:49 +0900

    新規：　Userクラス：のテスト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email returns name if a user has name' do
+    user = User.new(email: 'alice@example.com', name: 'Alice')
+
+    assert_equal 'Alice', user.name_or_email
+  end
+
+  test '#name_or_email returns email if a user does not have name' do
+    user = User.new(email: 'alice@example.com', name: nil)
+    assert_equal 'alice@example.com', user.name_or_email
+  end
 end
