9395d53139939aa9d0747a508cf475764a70cd32
masyuko0222 <masyuko0222@gmail.com>
2023-06-11 16:46:20 +0900

    新規: システムテストの作成。日報の新規作成、編集(更新)、削除

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -3,45 +3,51 @@
 require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
+  include Warden::Test::Helpers
+
   setup do
-    @report = reports(:one)
+    @user = FactoryBot.create(:user, email: 'alice@example.com')
+    login_as(@user)
   end
 
   test 'visiting the index' do
     visit reports_url
-    assert_selector 'h1', text: 'Reports'
+    assert_selector 'h1', text: '日報の一覧'
   end
 
-  test 'should create report' do
-    visit reports_url
-    click_on 'New report'
+  test 'create a new report' do
+    visit new_report_url
+    fill_in 'タイトル', with: 'First report'
+    fill_in '内容', with: 'はじめまして'
+    click_button '登録する'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
-
-    assert_text 'Report was successfully created'
-    click_on 'Back'
+    assert_text '日報が作成されました'
+    assert_text 'First report'
+    assert_text 'はじめまして'
+    assert_text "作成者: #{@user.name}"
   end
 
-  test 'should update Report' do
-    visit report_url(@report)
-    click_on 'Edit this report', match: :first
+  test 'update a report' do
+    report = FactoryBot.create(:report, user: @user)
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
+    visit edit_report_url(report.id)
+    fill_in 'タイトル', with: 'Updated Title'
+    fill_in '内容', with: 'Updated Content'
+    click_button '更新する'
 
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    assert_text '日報が更新されました'
+    assert_text 'Updated Title'
+    assert_text 'Updated Content'
+    assert_text "作成者: #{@user.name}"
   end
 
-  test 'should destroy Report' do
-    visit report_url(@report)
-    click_on 'Destroy this report', match: :first
+  test 'destory a report' do
+    report = FactoryBot.create(:report, user: @user)
 
-    assert_text 'Report was successfully destroyed'
+    visit report_url(report.id)
+
+    click_button 'この日報を削除'
+
+    assert_text '日報が削除されました。'
   end
 end
