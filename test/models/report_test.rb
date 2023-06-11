ec0f93c7f1490c338ed5f0884eadc6bf6f4f16f1
masyuko0222 <masyuko0222@gmail.com>
2023-06-11 20:20:09 +0900

    リファクタリング： Reportモデルテストのリファクタリング
    ・必要なレポートオブジェクトをsetupで生成
    ・user_idで関連付けられているuserを指定していたが、userで指定するように

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,9 +4,16 @@ require 'test_helper'
 require 'debug'
 
 class ReportTest < ActiveSupport::TestCase
+  setup do
+    FactoryBot.create(:report, id: 10)
+    FactoryBot.create(:report, id: 11)
+    FactoryBot.create(:report, id: 12)
+    FactoryBot.create(:report, id: 13)
+  end
+
   test '#editable? should return true when target user tries to edit a report created by target user' do
     target_user = FactoryBot.create(:user)
-    report = FactoryBot.create(:report, user_id: target_user.id)
+    report = FactoryBot.create(:report, user: target_user)
 
     assert report.editable?(target_user)
   end
@@ -14,7 +21,7 @@ class ReportTest < ActiveSupport::TestCase
   test '#editable? should return false when target user tries to edit a report created by another user' do
     target_user = FactoryBot.create(:user)
     another_user = User.create(email: 'bob@example.com', name: 'Bob', password: 'password')
-    report = FactoryBot.create(:report, user_id: another_user.id)
+    report = FactoryBot.create(:report, user: another_user)
 
     assert_not report.editable?(target_user)
   end
@@ -26,9 +33,6 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#save_mentions should save new mentioning relationships after saving when a new report is created with mentions' do
-    FactoryBot.create(:report, id: 10)
-    FactoryBot.create(:report, id: 11)
-
     report = FactoryBot.build(:report)
     report.content = 'http://localhost:3000/reports/10 is good. http://localhost:3000/reports/11 is bad.'
     report.save
@@ -37,11 +41,6 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#save_mentions should destory exisiting mentioning relationships and save new mentioning relationships after saving when a report is updated' do
-    FactoryBot.create(:report, id: 10)
-    FactoryBot.create(:report, id: 11)
-    FactoryBot.create(:report, id: 12)
-    FactoryBot.create(:report, id: 13)
-
     report = FactoryBot.create(:report, content: 'http://localhost:3000/reports/10 is good. http://localhost:3000/reports/11 is bad.')
 
     assert_equal [10, 11], report.mentioning_report_ids
@@ -52,12 +51,7 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#save_mentions should save new mentioning relationships without mention to reportself after saving' do
-    FactoryBot.create(:report, id: 10)
-    FactoryBot.create(:report, id: 11)
-
-    content_mentioning_reportself = 'http://localhost:3000/reports/10 is good. http://localhost:3000/reports/11 is bad. http://localhost:3000/reports/99 is me.'
-
-    report = FactoryBot.create(:report, id: 99, content: content_mentioning_reportself)
+    report = FactoryBot.create(:report, id: 99, content: 'http://localhost:3000/reports/10 is good. http://localhost:3000/reports/11 is bad. http://localhost:3000/reports/99 is me.')
 
     assert_equal [10, 11], report.mentioning_report_ids
   end
