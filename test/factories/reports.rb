8e7b6dbb89988a553d9743a3f27be1a799b4bc7b
masyuko0222 <masyuko0222@gmail.com>
2023-06-11 11:55:37 +0900

    新規: Reportクラスのeditable?メソッドのテストを作成

---
+++ test/factories/reports.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :report do
+    title { 'Test Title' }
+    content { 'Test Content' }
+  end
+end
--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable? should return true when target user tries to edit a report created by target user' do
+    target_user = FactoryBot.create(:user)
+    report = FactoryBot.create(:report, user_id: target_user.id)
+
+    assert report.editable?(target_user)
+  end
+
+  test '#editable? should return false when target user tries to edit a report created by another user' do
+    target_user = FactoryBot.create(:user)
+    another_user = User.create(email: 'bob@example.com', name: 'Bob', password: 'password')
+    report = FactoryBot.create(:report, user_id: another_user.id)
+
+    assert_not report.editable?(target_user)
+  end
 end
