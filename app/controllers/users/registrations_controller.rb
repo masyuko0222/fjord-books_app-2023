5abe746f6a4533ccd82beabee9ceb3dadd12f34d
masyuko0222 <masyuko0222@gmail.com>
2024-08-27 20:35:45 +0900

    Github登録ユーザーはパスワードなしでアカウント編集ができるようにした
    - パスワードの変更のみ、現在のパスワードが必要

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def update_resource(resource, params)
+    return super if params['password'].present?
+
+    resource.update_without_password(params.except(:current_password))
+  end
+end
