d32e54e7030c6ccb4bfd1a3f2d169fba143a03b7
masyuko0222 <masyuko0222@gmail.com>
2024-08-27 21:07:09 +0900

    利用していないデータなので削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,7 +10,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'github') if is_navigational_format?
     else
-      session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url
     end
   end
